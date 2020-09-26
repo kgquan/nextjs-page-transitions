1a6fc05d1915d4d7fd619f426d859ded4f2f4dda
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2020-09-26 17:28:18 -0600

    Add animations to product page

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -2,42 +2,72 @@ import fetch from "isomorphic-unfetch";
 import Link from "next/link";
 import { motion } from 'framer-motion';
 
+const easing = [0.6, -0.05, 0.01, 0.99];
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duraiton: 0.6,
+      ease: easing
+    }
+  }
+};
+
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+}
+
 const Product = props => (
-  <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+  <motion.div exit={{ opacity: 0 }} initial='initial' animate='animate'>
     <div className='fullscreen'>
       <div className='product'>
         <div className='img'>
-          <img key={props.product.image} src={props.product.image} />
+          <motion.img 
+            initial={{ x: 20, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.2 }}
+            key={props.product.image} 
+            src={props.product.image} />
         </div>
         <div className='product-details'>
-          <div className='inner'>
+          <motion.div variants={stagger} className='inner'>
             <Link href='/'>
-              <div>
+              <motion.div variants={fadeInUp}>
                 <a className='go-back'>Back to products</a>
-              </div>
+              </motion.div>
             </Link>
-            <div>
+            <motion.div variants={fadeInUp}>
               <span className='category'>Protein</span>
-            </div>
-            <h1>{props.product.name}</h1>
-            <p>{props.product.details}</p>
-            <div className='additonals'>
+            </motion.div>
+            <motion.h1 variants={fadeInUp}>{props.product.name}</motion.h1>
+            <motion.p variants={fadeInUp}>{props.product.details}</motion.p>
+            <motion.div variants={fadeInUp} className='additonals'>
               <span>Soy Free</span>
               <span>Gluten Free</span>
-            </div>
-            <div className='qty-price'>
+            </motion.div>
+            <motion.div variants={fadeInUp} className='qty-price'>
               <div className='qty'>
                 <div className='minus'>-</div>
                 <div className='amount'>1</div>
                 <div className='add'>+</div>
               </div>
               <span className='price'>{props.product.price}</span>
-            </div>
-            <div className='btn-row'>
+            </motion.div>
+            <motion.div variants={fadeInUp} className='btn-row'>
               <button className='add-to-cart'> Add to cart</button>
               <button className='subscribe'> Subscribe</button>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </div>
       </div>
     </div>
